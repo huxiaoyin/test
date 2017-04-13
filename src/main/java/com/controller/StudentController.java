42d640e717425d2202814634530b2f8f1b5299ce
huxiaoyin <2392506647@qq.com>
2017-04-13 23:07:20 +0800

    update ssm

---
+++ src/main/java/com/controller/StudentController.java
@@ -0,0 +1,51 @@
+package com.controller;
+
+import com.dao.StudentMapper;
+import com.pojo.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/4/13.
+ */
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+    @Autowired
+    private StudentMapper studentMapper;
+
+    @RequestMapping("/get")
+    @ResponseBody
+    public String getStu() {
+        System.out.print("Test------------------------->Hello");
+        return "HELLO,get student";
+    }
+
+    @RequestMapping("/insertTest")
+    public String insertTest() {
+        List<Student> students = new ArrayList<>();
+
+        for (int j = 0; j < 10; j++) {
+
+            for (int i = 0; i < 25000; i++) {
+                Student s = new Student();
+                s.setSname("hxy" + (int) (Math.random() * 9000));
+                s.setCid(i % 2 == 0 ? 1 : 2);
+                students.add(s);
+
+            }
+
+            studentMapper.insertList(students);
+            students.clear();
+        }
+
+        return "ok!!!!!!!";
+    }
+
+}
